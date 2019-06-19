bbe418bbc93ede8cea4fa4cde6b3b8bcc43224ac
hongzicong <973138379@qq.com>
2019-06-19 16:00:25 +0800

    hzc second iter add table user wallet task accepttask

--- BackEnd/account_info/models.py
+++ BackEnd/account_info/models.py
@@ -7,3 +7,37 @@ class account_info(models.Model):
     phone = models.CharField(max_length=string_max_length, unique=True)
     password = models.CharField(max_length=string_max_length)
     avatar = models.CharField(max_length=string_max_length, default='')
+
+
+class User(models.Model):
+    userID = models.AutoField(unique=True, primary_key=True)
+    userName = models.CharField(max_length=100, unique=True)
+    gender = models.BooleanField()
+    email = models.CharField(max_length=50)
+    phone = models.CharField(max_length=15)
+    school = models.CharField(max_length=50)
+    major = models.CharField(max_length=30)
+    age = models.IntegerField()
+    password = models.CharField(max_length=30)
+
+class Wallet(models.Model):
+    walletID = models.AutoField(unique=True, primary_key=True)
+    balance = models.IntegerField()
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+
+class Task(models.Model):
+    taskID = models.AutoField(unique=True, primary_key=True)
+    taskName = models.CharField(max_length=50)
+    content = models.CharField(max_length=300)
+    type = models.IntegerField()
+    issuerID = models.ForeignKey(User, on_delete=models.CASCADE)
+    reward = models.IntegerField()
+    deadline = models.DateTimeField()
+    isCompleted = models.BooleanField()
+    repeatTime = models.IntegerField()
+
+class AcceptTask(models.Model):
+    acceptTaskID = models.AutoField(unique=True, primary_key=True)
+    userID = models.ForeignKey(User, on_delete=models.CASCADE)
+    taskID = models.ForeignKey(Task, on_delete=models.CASCADE)
+    isFinished = models.BooleanField()
