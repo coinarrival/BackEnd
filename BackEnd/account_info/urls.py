4880f5c0d4d3f1f8dc8471cddad45aca9c652077
zhangflu <423127555@qq.com>
2019-06-26 08:20:02 +0800

    fix failed

--- BackEnd/account_info/urls.py
+++ BackEnd/account_info/urls.py
@@ -6,15 +6,13 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('verification', views.verification, name='verification'),
     path('account_info', views.operate_account_info, name='account_info'),
-    path('registration', views.registration, name='registration'),
-    """
-    path('balance', views.get_balance, name='balance'), 
-    path('task', views.operate_task, name='task'), 
-    path('tasks', views.get_tasks, name='tasks'), 
-    path('task_removed', views.task_finished, name='task_finished'),
-    path('created_tasks', views.operate_created_tasks, name='created_tasks'),
-    path('accepted_tasks', views.operate_accepted_tasks, name='accepted_tasks'),
-    path('acceptance', views.operate_acceptance, name='acceptance'),
-    path('acceptance_removed', views.acceptance_removed, name='acceptance_removed'),
-    """
+    path('registration', views.registration, name='registration')
+    # path('balance', views.get_balance, name='balance'), 
+    # path('task', views.operate_task, name='task'), 
+    # path('tasks', views.get_tasks, name='tasks'), 
+    # path('task_removed', views.task_finished, name='task_finished'),
+    # path('created_tasks', views.operate_created_tasks, name='created_tasks'),
+    # path('accepted_tasks', views.operate_accepted_tasks, name='accepted_tasks'),
+    # path('acceptance', views.operate_acceptance, name='acceptance'),
+    # path('acceptance_removed', views.acceptance_removed, name='acceptance_removed'),
 ]
